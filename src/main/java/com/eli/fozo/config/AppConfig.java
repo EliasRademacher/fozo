7471b32f794600f13a5046230ece219a6d46666b
EliasRademacher <rademace@onid.oregonstate.edu>
2016-10-05 15:33:46 -0700

    Initial commit. Based largely on the exmples found on https://github.com/freemarker/freemarker-tutorials and https://github.com/GoogleCloudPlatform/getting-started-java.

---
+++ src/main/java/com/eli/fozo/config/AppConfig.java
@@ -0,0 +1,10 @@
+package com.eli.fozo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.eli.fozo")
+public class AppConfig {
+
+}
---
+++ src/main/java/com/eli/fozo/controller/HelloWorldController.java
@@ -0,0 +1,22 @@
+package com.eli.fozo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    public String loadExample(Model model) {
+
+        // Send the variable "pageTitle" to the view.
+        // This can be accessed by ${pageTitle} in the FreeMarker file "hello-world.ftl"
+        model.addAttribute("pageTitle", "Fozo, Baby");
+
+        // When the user navigates to http://<deploy-url>/<context>/, tell the server to use
+        // `/WEB-INF/ftl/views/hello-world.ftl` to render the view
+        return "hello-world";
+    }
+}
