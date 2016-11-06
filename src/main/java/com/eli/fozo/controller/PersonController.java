1b4cb956e0301be215d98253fa0f2766af56d2de
EliasRademacher <rademace@onid.oregonstate.edu>
2016-11-06 09:36:55 -0800

    Added a simple GET endpoint to return a string, to help with development of client.

--- src/main/java/com/eli/fozo/controller/PersonController.java
+++ src/main/java/com/eli/fozo/controller/PersonController.java
@@ -32,6 +32,12 @@ public class PersonController {
         datastore.put(defaultGroup);
     }
 
+    @RequestMapping(value="/quote", method=RequestMethod.GET)
+    public String getQuote() {
+        String quote = "Twenty years from now you will be more disappointed by the things that you didn't do than by the ones you did do. So throw off the bowlines. Sail away from the safe harbor. Catch the trade winds in your sails. Explore. Dream. Discover.";
+        return quote;
+    }
+
     @RequestMapping(value="/people", method=RequestMethod.GET)
     public ResponseEntity<List<Person>> getPeople() {
         Query personQuery = new Query("Person");
